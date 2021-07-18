044b2584128335b9498085f57b8a45ae3eaff792
giocast <42517816+giocast@users.noreply.github.com>
2021-07-18 20:53:29 +0200

    Update caseA.py

--- caseA.py
+++ caseA.py
@@ -579,7 +579,7 @@ def main() -> None:
     #print(len(matrixRecomm))
     
     # Create the Updater and pass it the bot's token.
-    updater = Updater("1867453248:AAF_1-hJUc709RP6Pmi5PycSZm_OxmMqV2g")
+    updater = Updater("ggggggggg")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
